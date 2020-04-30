ee531b1ceabd0db62ab111c476b1a719592dec9c
edgraham <egraham147@gmail.com>
2020-04-30 12:04:24 -0700

    concat script repair for python3

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='Binsanity',    # This is the name of your PyPI-package.
-    version='0.3.7',
+    version='0.3.8',
     description='Method to cluster contigs based a biphasic method with coverage and composition',
     url="https://github.com/edgraham/BinSanity",
     author="Elaina Graham",
     author_email="egraham147@gmail.com",
     license="GPL3",
     install_requires=['biopython','pandas>=0.13.0','scipy>=0.13.0','checkm-genome'],
-    scripts=['utils/concat','utils/simplify-fasta','utils/get-ids','bin/Binsanity-lc','bin/Binsanity','bin/Binsanity-refine','bin/Binsanity-wf',"bin/Binsanity-profile","utils/transform-coverage-profile","utils/bin_evaluation","utils/checkm_analysis"]      ,
+    scripts=['utils/identifyHMM','utils/concat','utils/simplify-fasta','utils/get-ids','bin/Binsanity-lc','bin/Binsanity','bin/Binsanity-refine','bin/Binsanity-wf',"bin/Binsanity-profile","utils/transform-coverage-profile","utils/bin_evaluation","utils/checkm_analysis"]      ,
 )
