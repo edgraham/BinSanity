b6b0f8759b6f750350f8225ee18a1a7860f86882
edgraham <egraham147@gmail.com>
2020-06-08 09:14:04 -0700

    fix

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='Binsanity',    # This is the name of your PyPI-package.
-    version='0.4.2',
+    version='0.4.3',
     description='Method to cluster contigs based a biphasic method with coverage and composition',
     url="https://github.com/edgraham/BinSanity",
     author="Elaina Graham",
     author_email="egraham147@gmail.com",
     license="GPL3",
-    install_requires=['biopython','pandas>=0.13.0','scipy>=0.13.0','checkm-genome','sklearn>=0.23'],
+    install_requires=['biopython','pandas>=0.13.0','scipy>=0.13.0','checkm-genome','scikit-learn>=0.23'],
     scripts=['utils/identifyHMM','utils/concat','utils/simplify-fasta','utils/get-ids','bin/Binsanity-lc','bin/Binsanity','bin/Binsanity-refine','bin/Binsanity-wf',"bin/Binsanity-profile","utils/transform-coverage-profile","utils/bin_evaluation","utils/checkm_analysis"]      ,
 )
