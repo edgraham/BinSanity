08293644cfd319e92c202d9d7ccd8e10274b5780
edgraham <egraham147@gmail.com>
2021-03-25 08:21:42 -0700

    v0.5.2

---
+++ utils/makeFileTabDelimited.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+import sys
+handle = open(sys.argv[1],"r")
+readlines = [filter(None,line.replace("\n","").split(" ")) for line in handle.readlines()]
+print '\n'.join(map('\t'.join,readlines))
