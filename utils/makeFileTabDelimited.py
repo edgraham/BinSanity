abf77a86f6a073b49443c5dff0f1583f3869f4df
edgraham <egraham147@gmail.com>
2021-03-25 08:29:33 -0700

    v0.5.2

--- utils/makeFileTabDelimited.py
+++ utils/makeFileTabDelimited.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 import sys
+import pandas as pd
 handle = open(sys.argv[1],"r")
 readlines = [filter(None,line.replace("\n","").split(" ")) for line in handle.readlines()]
-print '\n'.join(map('\t'.join,readlines))
+print('\n'.join(map('\t'.join,readlines)))
+
